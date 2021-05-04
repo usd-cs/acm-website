805599e7c8f20fcfdaca530778a2aeaebe1165d2
guiOsorio <gosorio@sandiego.edu>
2021-05-03 17:35:46 -0700

    added styling to the home page

--- frontend/src/components/Pages/Home.js
+++ frontend/src/components/Pages/Home.js
@@ -12,7 +12,7 @@ class Home extends Component {
         acmDescription: "What is ACM USD?"
     }
 
-    setAnswer = () => this.setState({ default: false, acmDescription: "ACM is an international community for anyone interested in computing. USD's chapter is for all students interested in computing, regardless of major or minor. If you are interested in collaborating with new people, enhacing your interviewing skills, networking with professionals in the field, resume workshops, or just hanging out with other students interested in computers, then join ACM! Meetings are held biweekly." })
+    setAnswer = () => this.setState({ default: false, acmDescription: "\"ACM is an international community for anyone interested in computing. USD's chapter is for all students interested in computing, regardless of major or minor. If you are interested in collaborating with new people, enhacing your interviewing skills, networking with professionals in the field, resume workshops, or just hanging out with other students interested in computers, then join ACM! Meetings are held biweekly.\"" })
 
     setQuestion = () => this.setState({ default: true, acmDescription: "What is ACM USD?" })
 
@@ -20,39 +20,41 @@ class Home extends Component {
         const { defaultText, acmDescription } = this.state;
         return (
                 <div  className="home container">
-                    <div className="page-title">
-                        <h1>Welcome to USD's Association for Computing Machinery</h1>
-                    </div>
-                    <div className="acm-description" onMouseOver={this.setAnswer} onMouseLeave={this.setQuestion}>
-                        <p>{acmDescription}</p>
-                    </div>
-                    <div className="members">
+                    <section className="welcome">
+                        <div className="page-title">
+                            <h1>Welcome to USD's Association for Computing Machinery</h1>
+                        </div>
+                        <div className="acm-description" onMouseOver={this.setAnswer} onMouseLeave={this.setQuestion}>
+                            <p>{acmDescription}</p>
+                        </div>
+                    </section>
+                    <section className="members">
                         <h2>Exec Members</h2>
                         <figure className="figure">
-                            <div className="members-images">
-                                <div className="member">
-                                    <img src={ava} width="220" height="220" alt="" />
+                            <div className="members-images row">
+                                <div className="member col-10 col-md-4">
+                                    <img src={ava} alt="" />
                                     <a href="mailto: agerami@sandiego.edu">Email Ava</a>
                                 </div>
-                                <div className="member">
-                                    <img src={shaydon} width="220" height="220" alt="" />
+                                <div className="member col-10 col-md-4">
+                                    <img src={shaydon} alt="" />
                                     <a href="mailto: sbodemar@sandiego.edu">Email Shaydon</a>
                                 </div>
-                                <div className="member">
-                                    <img src={sarah} width="220" height="220" alt="" />
+                                <div className="member col-10 col-md-4">
+                                    <img src={sarah} alt="" />
                                     <a href="mailto: shaetzel@sandiego.edu">Email Sarah</a>
                                 </div>
-                                <div className="member">
-                                    <img src={tatiana} width="220" height="220" alt="" />
+                                <div className="member col-10 col-md-4">
+                                    <img src={tatiana} alt="" />
                                     <a href="mailto: tbarbone@sandiego.edu">Email Tatiana</a>
                                 </div>
-                                <div className="member">
-                                    <img src={rachel} width="220" height="220" alt="" />
+                                <div className="member col-10 col-md-4">
+                                    <img src={rachel} alt="" />
                                     <a href="mailto: rvaldezdiaz@sandiego.edu">Email Rachel</a>
                                 </div>
                             </div>
                         </figure>
-                    </div>
+                    </section>
                     <div className="social-media">
                         <h2> Social Media</h2>
                         <div className="social-icons">
