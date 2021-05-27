b2e499f159229d4167a9246ab0a749887f76581e
katrinamb <katfairwind@hotmail.com>
2021-05-27 14:22:04 -0700

    practice

--- frontend/src/components/Pages/Entertainment.js
+++ frontend/src/components/Pages/Entertainment.js
@@ -6,6 +6,7 @@ class Entertainment extends Component {
         return (
             <div  className="entertainment container">
                 <h1>ENTERTAINMENT PAGE</h1>
+                <p>Katrina</p>
             </div>
         );
     }
