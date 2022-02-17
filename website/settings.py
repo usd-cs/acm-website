850757c2eb69a706f2f520150462a34a807f523e
guiOsorio <gosorio@sandiego.edu>
2022-02-16 17:57:29 -0800

    STATIC_ROOT added

--- website/settings.py
+++ website/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = '/ static /'
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT,'static')
+
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
     '/acm-website/frontend/static/'
